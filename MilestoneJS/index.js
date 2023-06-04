a3759fa507721554339e7f3f9e24a444e7dcbfcc
abirkholt <abirkholt@gmail.com>
2023-06-04 10:17:39 -0700

    the grid loads

--- MilestoneJS/index.js
+++ MilestoneJS/index.js
@@ -3,6 +3,8 @@ var tileSelected = null;
 
 var error = 0;
 
+
+// starting board to load in
 var board = [
     "-5-314-6-",
     "87---94-3",
@@ -15,6 +17,7 @@ var board = [
     "-3-1-867-",
 ]
 
+// the board compared to during game
 var solution = [
     "259314768",
     "871629453",
@@ -26,3 +29,32 @@ var solution = [
     "184796325",
     "532148679",
 ]
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    // for loop to load 1-9 instead of having it based in html
+    for (let i = 1; i<=9; i++) {
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i;
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+
+    //board grid
+    // r = row c = coulum
+    for (let r = 0; r <9; r++) {
+        for (let c=0; c < 9; c++) {
+            let tile = document.createElement("div");
+            // "-" each is being loaded as Row - column to create each square
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
