6e6c0790671efd96a7973e624047f99968fd1d4b
abirkholt <abirkholt@gmail.com>
2023-06-07 19:01:05 -0700

    cleaned up

--- MilestoneJS/index.js
+++ MilestoneJS/index.js
@@ -1,126 +1,126 @@
-var numSelected = null;
-var tileSelected = null;
+// Variables
+var numSelected = null; // The currently selected number
+var tileSelected = null; // The currently selected tile
+var error = 0; // Number of mistakes made by the user
 
-var error = 0;
-
-
-// starting board to load in
+// Initial board configuration
 var board = [
-    "-5-314-6-",
-    "87---94-3",
-    "6435-7192",
-    "--78-521-",
-    "41-9-----",
-    "-25-619-7",
-    "79-25-84-",
-    "--4-96--5",
-    "-3-1-867-",
-]
+  "-5-314-6-",
+  "87---94-3",
+  "6435-7192",
+  "--78-521-",
+  "41-9-----",
+  "-25-619-7",
+  "79-25-84-",
+  "--4-96--5",
+  "-3-1-867-",
+];
 
-// the board compared to during game
+// Solution for the Sudoku puzzle
 var solution = [
-    "259314768",
-    "871629453",
-    "643587192",
-    "967835214",
-    "418972536",
-    "325461987",
-    "796253841",
-    "184796325",
-    "532148679",
-]
+  "259314768",
+  "871629453",
+  "643587192",
+  "967835214",
+  "418972536",
+  "325461987",
+  "796253841",
+  "184796325",
+  "532148679",
+];
+
+// Track which numbers have been used in each column and box
+var usedNumbersInColumn = Array.from({ length: 9 }, () => Array.from({ length: 10 }, () => false));
+var usedNumbersInBox = Array.from({ length: 3 }, () =>
+  Array.from({ length: 3 }, () => Array.from({ length: 10 }, () => false))
+);
 
 window.onload = function () {
-    setGame();
-}
+  setGame(); // Initialize the Sudoku game
+};
 
+// Set up the Sudoku game
 function setGame() {
-    // for loop to load 1-9 instead of having it based in html
-    for (let i = 1; i <= 9; i++) {
-        let number = document.createElement("div");
-        number.id = i;
-        number.innerText = i;
-        number.addEventListener("click", selectNumber);
-        number.classList.add("number");
-        document.getElementById("digits").appendChild(number);
-    }
-
-
-    //board grid
-    // r = row c = coulum
-    for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
-            let tile = document.createElement("div");
-            // "-" each is being loaded as Row - column to create each square
-            tile.id = r.toString() + "-" + c.toString();
-            // loading inner text and leaving blank where "-" is found
-            if (board[r][c] != "-") {
-                tile.innerText = board[r][c];
-                tile.classList.add("tile-start");
-            }
-            // checks for Row and coulmn to add darker line
-            if (r == 2 || r == 5) {
-                tile.classList.add("horizontal-line");
-            }
-            if (c == 2 || c == 5) {
-                tile.classList.add("vertical-line");
-            }
-            tile.addEventListener("click", selectTile);
-            tile.classList.add("tile");
-            document.getElementById("board").append(tile);
-        }
-    }
+  createNumberButtons(); // Create number buttons for user selection
+  createBoard(); // Create the Sudoku board
 }
 
-// TODO: add effects to board when the same number Selected
+// Create number buttons for user selection
+function createNumberButtons() {
+  for (let i = 1; i <= 9; i++) {
+    let number = document.createElement("div");
+    number.id = i;
+    number.innerText = i;
+    number.addEventListener("click", selectNumber);
+    number.classList.add("number");
+    document.getElementById("digits").appendChild(number);
+  }
+}
+
+// Create the Sudoku board
+function createBoard() {
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      if (board[r][c] != "-") {
+        tile.innerText = board[r][c];
+        tile.classList.add("tile-start");
+        updateUsedNumbers(r, c, board[r][c]);
+      }
+      if (r == 2 || r == 5) {
+        tile.classList.add("horizontal-line");
+      }
+      if (c == 2 || c == 5) {
+        tile.classList.add("vertical-line");
+      }
+      tile.addEventListener("click", selectTile);
+      tile.classList.add("tile");
+      document.getElementById("board").append(tile);
+    }
+  }
+}
+
+// Handle number selection by the user
 function selectNumber() {
-    if (numSelected != null) {
-        numSelected.classList.remove("number-selected");
-    }
-    numSelected = this;
-    numSelected.classList.add("number-selected")
+  if (numSelected != null) {
+    numSelected.classList.remove("number-selected");
+  }
+  numSelected = this;
+  numSelected.classList.add("number-selected");
 }
 
+// Handle tile selection by the user
 function selectTile() {
-    if (numSelected) {
-        // prevents overiding numbers by checking inner text value
-        if (this.innerText != "") {
-            return;
-        }
-
-        // using coords to locate the value of the solution
-        let coords = this.id.split("-");
-        let r = parseInt(coords[0]);
-        let c = parseInt(coords[1]);
-
-        if (solution[r][c] == numSelected.id) {
-            this.innerText = numSelected.id;
-        }
-        else {
-            error += 1;
-            document.getElementById("error").innerText = error;
-        }
-
+  if (numSelected) {
+    if (this.innerText != "") {
+      return; // Prevent overriding existing numbers
     }
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+    if (solution[r][c] == numSelected.id && !isNumberUsedInColumn(c, numSelected.id) && !isNumberUsedInBox(r, c, numSelected.id)) {
+      this.innerText = numSelected.id; // Fill in the selected number
+      updateUsedNumbers(r, c, numSelected.id); // Update used numbers in column and box
+    } else {
+      error += 1;
+      document.getElementById("error").innerText = error; // Increment error count
+    }
+  }
 }
 
-//TODO: list
+// Update used numbers in the column and box
+function updateUsedNumbers(row, column, number) {
+  usedNumbersInColumn[column][number] = true;
+  usedNumbersInBox[Math.floor(row / 3)][Math.floor(column / 3)][number] = true;
+}
 
-//score counting
+// Check if a number is already used in the column
+function isNumberUsedInColumn(column, number) {
+  return usedNumbersInColumn[column][number];
+}
 
-// points generated by correct number in grid
-
-//timer based muiltplier for points
-
-//a timer, should go into on window load  
-//startTimer();
-// 
-
-// if errors excede 3 game over
-//id("errors").textcontent = "Mistakes" + error + "/3"
-
-// pop up to reset board on errors = 3
-
-// pop up for win when board = solution
-
-// both with working reset function
+// Check if a number is already used in the 3x3 box
+function isNumberUsedInBox(row, column, number) {
+  return usedNumbersInBox[Math.floor(row / 3)][Math.floor(column / 3)][number];
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Milestone1
+Sudoku is played on a grid of 9 x 9 spaces. Within the rows and columns are 9 “squares” (made up of 3 x 3 spaces). Each row, column and square (9 spaces each) needs to be filled out with the numbers 1-9, without repeating any numbers within the row, column or square. 
+
+Sudoku Rule № 1: Use Numbers 1-9
+Sudoku Rule № 2: Don’t Repeat Any Numbers
+Sudoku Rule № 3: Don’t Guess
+Sudoku Rule № 4: Use Process of Elimination
+
+
+cited base code
+**/ * Title: Sudoku * Author:(ImKennyYip) * Date: (2021) * Code version:(v1) * Availability:https://github.com/ImKennyYip/Sudoku * **/
